e103d25be54e6e7b65dba1704b9d36303b93c4ea
David Yesil <David.Yesil@hs-augsburg.de>
2017-10-31 20:24:22 +0100

    added build cython to buildscript

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
 # Shell script to build the Connect Four Linux executable.
 # The resulting "connectfour_linux" script will be available in the "dist" directory.
-
+bash cython_linux.sh
 pyinstaller \
     --clean --noconfirm --onefile \
     --log-level=WARN \
