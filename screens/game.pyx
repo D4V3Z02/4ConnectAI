a20a2e52fcd9bbd58311afb902fe09b2fea7438f
David Yesil <David.Yesil@hs-augsburg.de>
2017-10-31 22:07:46 +0100

    added scrum_mix :D

--- screens/game.pyx
+++ screens/game.pyx
@@ -67,7 +67,8 @@ cdef class Game:
                 self.board[x].insert(y, settings.EMPTY_SYMBOL)
                 self.highlighted_chips[x].insert(y, settings.EMPTY_SYMBOL)
         logging.info('Loading random music')
-        utils.load_random_music(['techno_dreaming.wav', 'techno_celebration.wav', 'electric_rain.wav', 'snake_trance.wav'], volume=self.musics_volume)
+        #utils.load_random_music(['techno_dreaming.wav', 'techno_celebration.wav', 'electric_rain.wav', 'snake_trance.wav'], volume=self.musics_volume)
+        utils.load_random_music(['scrum_mix.ogg', 'scrum_ost.ogg'], volume=self.musics_volume)
 
     cdef execute_update_dependant_on_state(self):
         self.state_mapping[self.state]()
