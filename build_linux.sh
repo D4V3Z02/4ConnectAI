d64122659f077a529aaf2d51c0ac2b053a4c71b0
David Yesil <David.Yesil@hs-augsburg.de>
2017-11-01 11:16:17 +0100

    fixed build-script for linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,18 @@
+#!/usr/bin/env bash
 # Shell script to build the Connect Four Linux executable.
 # The resulting "connectfour_linux" script will be available in the "dist" directory.
+#bash cython_linux.sh
 bash cython_linux.sh
 pyinstaller \
-    --clean --noconfirm --onefile \
+    --noconfirm \
     --log-level=WARN \
     --name="connectfour_linux" \
     --add-data="resources:resources" \
     run.py
+
+find . -name \*.so -exec cp {} ./dist/connectfour_linux \;
+mkdir ./dist/connectfour_linux/networking
+find ./networking -name \*.py -exec cp {} ./dist/connectfour_linux/networking \;
+mkdir ./dist/connectfour_linux/screens
+find ./screens -name \*.so -exec cp {} ./dist/connectfour_linux/screens \;
+find ./screens -name \*.py -exec cp {} ./dist/connectfour_linux/screens \;
