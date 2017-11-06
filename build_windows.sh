b15bea348d68f2816ec13bf2faa8cddb1970bfc7
David Yesil <David.Yesil@hs-augsburg.de>
2017-11-06 11:06:54 +0100

    rebuilt to oneliner

--- build_windows.sh
+++ build_windows.sh
@@ -1,4 +1,2 @@
 bash cython_windows.sh
-pyinstaller --clean --noconfirm --debug --log-level=WARN --name="connectfour_windows" \
- --icon="resources/images/icon.ico" --add-data="resources;resources" \
-  --add-data=".;." --add-data="screens;screens" run.py
+pyinstaller --clean --noconfirm --debug --log-level=WARN --name="connectfour_windows" --icon="resources/images/icon.ico" --add-data="resources;resources" --add-data=".;." --add-data="screens;screens" run.py
